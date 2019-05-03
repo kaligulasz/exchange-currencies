d87e0ac65f56e0b3bf328aa7a39c0e1404c03c5d
Daniel Kalus <daniel.kalus@performgroup.com>
2019-05-03 19:56:31 +0200

    loading: Add styles for requestLoader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Spinner5 } from 'styled-icons/icomoon/Spinner5';
+
+const Background = styled.div`
+  background: rgba(0, 0, 0, 0.7);
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  z-index: 1;
+`;
+
+const StyledSpinner = styled(Spinner5)`
+  width: 3.5rem;
+  animation: spin 1.2s linear infinite;
+  
+  @keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+`;
 
 const Loader = () => (
-  <div>loading...</div>
+  <Background><StyledSpinner /></Background>
 );
 
 export default Loader;
