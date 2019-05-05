130e09c0f24dec51b34e56a94694121aef5ce26b
Daniel Kalus <daniel.kalus@performgroup.com>
2019-05-05 20:50:50 +0200

    online-version

--- src/actions/mainPocketActions.js
+++
@@ -1,6 +0,0 @@
-export const UPDATE_MAIN_POCKET = 'UPDATE_MAIN_POCKET';
-
-export const updateMainPocket = pockets => ({
-  type: UPDATE_MAIN_POCKET,
-  pockets,
-});
--- src/sagas/index.js
+++
@@ -1,9 +0,0 @@
-import { all } from 'redux-saga/effects';
-
-import { watchUpdateCurrencyRates } from './currencyRatesSaga';
-
-export default function* rootSaga() {
-  yield all([
-    watchUpdateCurrencyRates(),
-  ]);
-}
